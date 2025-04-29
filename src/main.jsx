8f434298f6376e791cc18efffaf34caed1e53812
shovan-642 <shovan642@gmail.com>
2025-04-29 11:29:55 +0600

    footer, login, register page add

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './index.css'  
+import {
+  RouterProvider,
+} from "react-router-dom";
+import { router } from './routes/router.jsx';
+
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <div className='max-ww-screen-xl mx-auto'>
+    <RouterProvider router={router} />
+    </div>
   </StrictMode>,
 )
